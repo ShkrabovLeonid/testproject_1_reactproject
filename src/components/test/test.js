96812930c811ba6b955747950adf9532a5837c75
ShkrabovLeonid <l.shkrabov@gmail.com>
2021-06-09 08:02:50 +0300

    init

---
+++ src/components/test/test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import './test.css'
+
+class Test extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     years: 26,
+  //     name: 'NewName:' + this.props.name
+  //   }
+  //   this.nextYear = ()=>{
+  //     this.setState((state, props) =>{
+  //       return {
+  //         years: ++state.years,
+  //         name: props.name
+  //       };
+  //     })
+  //   }
+  // }
+
+  // or
+
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     years: 26,
+  //     name: 'NewName:' + this.props.name
+  //   }
+  //   this.nextYear = this.nextYear.bind(this); 
+  // }
+  // nextYear(){
+  //   this.setState((state, props) => {
+  //     return {
+  //       years: ++state.years,
+  //       name: props.name
+  //     };
+  //   });
+  // }
+
+  // or
+
+  state = {
+    years: 26,
+    name: 'NewName:' + this.props.name
+  }
+  nextYear = ()=> {
+    this.setState((state, props) => {
+      return {
+        years: ++state.years,
+        name: props.name
+      };
+    });
+  }
+  render(){
+    const {surname, link} = this.props,
+    {name, years} = this.state;
+    return (
+      <React.Fragment>
+      <button onClick={this.nextYear}></button>
+      <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+      <a href={link}>My profile</a>
+      </React.Fragment>
+    )
+  }
+}
+
+const All = ()=>{
+  return (
+    <>
+        <Test name='Leonid1' surname='Shkrabov' link='https://facebook.com' />,
+        <Test name='Leonid2' surname='Shkrabov' link='https://facebook.com' />,
+        <Test name='Leonid3' surname='Shkrabov' link='https://facebook.com' />,
+    </>
+  )
+};
+
+export default All;
